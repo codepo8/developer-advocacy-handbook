02a3831202e86bd13029e848ef95e334d9b28937
codepo8 <chris.heilmann@gmail.com>
2020-12-15 04:35:20 +0100

    lightbulb

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,2 +1,9 @@
 let bqs = document.querySelectorAll('blockquote');
 console.log(bqs);
+bqs.forEach(b => {
+  b.className = b.
+    querySelector('strong').
+      innerText.
+        toLowerCase().
+          replace(':','');
+});
