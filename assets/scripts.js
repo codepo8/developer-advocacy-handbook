093c543425178468f96840ff2adacc9410ec2656
codepo8 <chris.heilmann@gmail.com>
2020-12-27 13:52:45 +0100

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,10 +11,9 @@ const audiolink = document.querySelector('.audio a');
 if (audiolink) {
   audiolink.addEventListener('click', e => {
     e.preventDefault();
-    console.log(e.target.href);
     let a = document.createElement('audio');
-    a.src = e.target.href;
+    a.src = audiolink.href;
     a.width = "100%";
-    e.target.parentNode.replaceChild(e.target, a);
+    audiolink.parentNode.replaceChild(audiolink, a);
   })
 }
