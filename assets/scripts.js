3d48c92730d5ebe3bb1d0790765bac1648761ffb
codepo8 <chris.heilmann@gmail.com>
2020-12-27 13:49:29 +0100

    player

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,6 +11,11 @@ const audiolink = document.querySelector('.audio a');
 if (audiolink) {
   audiolink.addEventListener('click', e => {
     console.log(e.target.href);
+    let a = document.createElement('audio');
+    a.src = e.target.href;
+    a.width = "100%";
+    e.target.parentNode.addChild(a);
+    e.target.remove();
     e.preventDefault();
   })
 }
